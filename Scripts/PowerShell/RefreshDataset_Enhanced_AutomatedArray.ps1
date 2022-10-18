bc7700ad95ec3e2ec116bd18dea822b6637d7dfe
Vytautas Kraujalis <vytautas.kraujalis2@gmail.com>
2022-10-18 14:09:25 +0300

    added refresh check before first refresh request

--- Scripts/PowerShell/RefreshDataset_Enhanced_AutomatedArray.ps1
+++ Scripts/PowerShell/RefreshDataset_Enhanced_AutomatedArray.ps1
@@ -4,8 +4,12 @@
 # https://learn.microsoft.com/en-us/power-bi/connect-data/asynchronous-refresh
 
 # Fill in parameters
-$groupID = "Group ID" 
-$datasetID = "Dataset ID" 
+# ----------------------------------------------------------------------------
+$waitTime = 180 # <- how many seconds are we gonna wait between each api call.
+$groupID = "Group ID" # <- workspace id
+$datasetID = "Dataset ID" # <- dataset id
+
+# Request bodies, must be separated by ","
 $RequestBodies = @"
 {
     "type": "full",
@@ -36,7 +40,7 @@ $RequestBodies = @"
     ]
 }
 "@
-
+# ----------------------------------------------------------------------------
 
 # Base API for Power BI REST API
 $PbiRestApi = "https://api.powerbi.com/v1.0/myorg/"
@@ -45,11 +49,41 @@ $PbiRestApi = "https://api.powerbi.com/v1.0/myorg/"
 Write-Host -ForegroundColor White "Sign in to connect to the Power BI Service";
 Connect-PowerBIServiceAccount
 
-
-# TODO - firstly check if there's ongoing refreshes before starting the loop
-
 $iteration = 1
+$numerOfRequests = $RequestBodies.Length
 $RefreshURL = $PbiRestApi + "groups/" + $groupID + "/datasets/" + $datasetID + "/refreshes"
+
+# Before starting first refresh, we'll check if there's ongoing refresh. We'll keep looping thourgh until the latest refresh is completed or failed.
+$retries = 1
+DO
+{
+    # If we send the request not the first time, we'll introduce some delay
+    if ( $retries -ne 1 )
+    {
+        # To not use api each second, we will delay the requests
+        Start-Sleep -Seconds $waitTime
+    }
+
+    # Get the refresh history of the dataset 
+    $DatasetRefreshHistory = Invoke-PowerBIRestMethod -Method GET -Url $RefreshURL | ConvertFrom-Json
+    $refreshId = $DatasetRefreshHistory.value[0].requestId
+    $requestStatus = $DatasetRefreshHistory.value[0].status
+
+    Write-Host "The latest refresh id: $refreshId, status: $requestStatus."
+
+    $retries++
+} Until($requestStatus -ne "Unknown")
+
+# Check if the latest refresh is completed, failed or else
+if ($requestStatus -eq "Completed") {
+    Write-Host "The latest refresh is completed" -ForegroundColor Green
+} elseif ($requestStatus -eq "Failed") {
+    Write-Host "The latest refresh has failed" -ForegroundColor Red
+} else {
+    Write-Host "The latest refresh is unknown??" -ForegroundColor Yellow
+}
+
+Write-Host "`n Starting the refresh loop. There will be $numerOfRequests separate refreshes."
 
 # Perform a DO loop until we go through each of the request body from requestBodies array
 DO
@@ -58,7 +92,7 @@ DO
     
     # Extract a request body from array
     $RequestBody = $RequestBodies[$iteration-1]
-    Write-Host “Starting Refresh of the specified request body:`n $RequestBody”
+    Write-Host “`tStarting Refresh of the specified request body:`n $RequestBody”
 
     # Refresh the dataset
     Invoke-PowerBIRestMethod -Url $RefreshURL –Method POST –Verbose -Body $RequestBody  -ContentType "application/json"
@@ -68,7 +102,7 @@ DO
 
     # Retrieve the first refresh id
     $refreshId = $DatasetRefreshHistory.value[0].requestId
-    Write-Host "Refresh id: $refreshId `n"
+    Write-Host "`tRefresh id: $refreshId `n"
 
     # Url for refresh details of the specified refresh id
     $GetRefreshDetails = $PbiRestApi + "groups/" + $groupID + "/datasets/" + $datasetID + "/refreshes/" + $refreshId
@@ -79,29 +113,30 @@ DO
     DO
     {
         # To not use api each second, we will delay the requests
-        Start-Sleep -Seconds 180
+        Start-Sleep -Seconds $waitTime
 
         # Get the refresh details of the specified refresh id
-        Write-Host “Starting checking refresh details. Iteration - $iteration, retry - $retries”
+        Write-Host “`t`tStarting checking refresh details. Iteration - $iteration, retry - $retries”
         $RefreshDetails = Invoke-PowerBIRestMethod -Method GET -Url $GetRefreshDetails | ConvertFrom-Json
 
         # Get the status of the refresh
         $requestStatus = $RefreshDetails.status
         $DateTime = Get-Date
-        Write-Host “DateTime: $DateTime”
-        Write-Host "Refresh status: $requestStatus `n"
+        Write-Host “`t`tDateTime: $DateTime”
+        Write-Host "`t`tRefresh status: $requestStatus `n"
         $retries++
     } Until ($requestStatus -ne "Unknown")
     
     # Check if refresh completed, failed or else
     if ($requestStatus -eq "Completed") {
-        Write-Host "The refresh is completed" -ForegroundColor Green
+        Write-Host "`tThe refresh is completed" -ForegroundColor Green
     } elseif ($requestStatus -eq "Failed") {
-        Write-Host "The refresh has failed" -ForegroundColor Red
+        Write-Host "`tThe refresh has failed" -ForegroundColor Red
     } else {
-        Write-Host "The refresh is unknown??" -ForegroundColor Yellow
+        Write-Host "`tThe refresh is unknown??" -ForegroundColor Yellow
     }
 
     $iteration++
-} Until ($iteration -eq $RequestBodies.Length+1)
+} Until ($iteration -eq $numerOfRequests+1)
 
+Write-Host "All refreshes are done."
